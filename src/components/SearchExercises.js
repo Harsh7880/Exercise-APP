5278061b95f3f050eb0832c59109bea6816f6376
Harsh Kumar <harsh.kumar@robomq.io>
2022-08-24 22:36:54 +0530

    Banner Completed

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react';
+import { Box, Button, TextField, Stack, Typography } from '@mui/material';
+import { borderRadius } from '@mui/system';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{fontSize : { lg: '44px', xs: '30px'}}} mb="50px" textAlign="center">
+        Awesome Exercices You <br />
+        Should Know
+      </Typography>
+     <Box position="realtive" mb="72px">
+     <TextField
+     sx={{
+      input: {
+        fontWeight : '700',
+        border: 'none',
+        borderRadius: '4px'
+      },
+      width: { lg: '1170px', xs: '350px'},
+      backgroundColor: '#fff',
+      borderRadius: '40px'
+     }}
+     height="76px"
+     value=""
+     onChange={(e) => {}}
+     placeholder="Search Excercises"
+     type="text"
+     />
+ <Button className='search-btn' 
+ sx={{
+  bgcolor: '#FF2625',
+  color: '#fff',
+  textTransform: 'none'
+ }}
+ >Search</Button>
+     </Box>
+    </Stack>
   )
 }
 
